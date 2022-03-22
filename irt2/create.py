831daf2560980b86ebfc6edb99a9f9ef82f4b4af
Felix Hamann <felix@hamann.xyz>
2022-03-22 19:38:49 +0100

    added text sampling to create.py

--- irt2/create.py
+++ irt2/create.py
@@ -8,25 +8,34 @@ from irt2.graph import Graph
 from irt2.graph import Relation
 from irt2.types import VID, RID, EID, Triple, Mention
 
+from ktz.string import encode_line
 from ktz.dataclasses import Index
 from ktz.dataclasses import Builder
 from ktz.collections import buckets
 from ktz.collections import Incrementer
 from ktz.filesystem import path as kpath
+from ktz.multiprocessing import Relay
+from ktz.multiprocessing import Actor
+
+import yaml
+import spacy
 
 import re
 import csv
+import gzip
 import random
+import sqlite3
 import logging
 from pathlib import Path
 from itertools import islice
 from datetime import datetime
-from dataclasses import dataclass
 from dataclasses import field
+from dataclasses import dataclass
 from collections import Counter
 from collections import defaultdict
 from functools import cached_property
 
+from typing import Union
 from collections.abc import Iterable
 
 
@@ -109,6 +118,7 @@ class Match:
 
 
 # TODO use database connection for both matches/pages
+# (see bottom where text sampling takes place)
 def index_matches(path: Path, n: int = None):
     """
     Create an index of Match objects.
@@ -174,7 +184,8 @@ def get_mentions(
     log.info(f"pruning mentions to have at least {prune} matches")
 
     eid2mentions = {
-        eid: {mention: count for mention, count in counts.items() if count >= prune}
+        eid: {mention: count for mention,
+              count in counts.items() if count >= prune}
         for eid, counts in eid2mentions.items()
         if len(counts) > 0
     }
@@ -357,7 +368,8 @@ def split_mentions(
             continue
 
         # create a flat list to be split later
-        flat = {(vid, mention) for mention in mentions.eid2mentions[link].keys()}
+        flat = {(vid, mention)
+                for mention in mentions.eid2mentions[link].keys()}
 
         if vid in concept_vertices:
             split.closed_world_mentions.update(flat)
@@ -400,7 +412,7 @@ REL_SYNONYM = "IRT2:Synonym"
 
 # this functions assigns new gapless IDS starting at 0
 def split2irt2(config, split) -> IRT2:
-    """Transform a sampling.Split to an dataset.IRT2."""
+    """Transform a create.Split to an dataset.IRT2."""
     vids = Incrementer()
     rids = Incrementer()
     mids = Incrementer()
@@ -491,3 +503,216 @@ def split2irt2(config, split) -> IRT2:
     )
 
     return build()
+
+
+#
+# text sampling
+#
+
+# multiprocessing: using own multiprocessing
+# because the spacy pipeline n_process was unreliable
+# and slow (maybe I did it wrong...?)
+#
+#   how it works
+#
+# * dispatcher process creates
+# * 1 reader process:
+#   - iterate all pages from sqlite db
+# * 1 writer process:
+#   - open gzipped file
+#   - poll on writer queue to write csv lines
+# * n worker processes:
+#   - process them with spacy to detect sentence boundaries
+#   - match the sentences with an Index[Match] instance
+#   - send sentences to writer queue
+#
+#   the writer process _does not_ match mid's yet
+#   but builds a split-agnostic file which is then
+#   used to select contexts for specific splits and
+#   subsamples.
+
+class Reader(Actor):
+
+    db: str
+
+    def __init__(self, db: Union[Path, str]):
+        super().__init__()
+        self.db = db
+
+    def startup(self):
+        log.info(f"[{self.name}] connecting to db {self.db}")
+        self.con = sqlite3.connect(str(self.db))
+
+    def shutdown(self):
+        self.con.close()
+
+    def loop(self):
+        log.info(f"[{self.name}] reading from db")
+
+        cur = self.con.cursor()
+        rows = cur.execute(
+            "select text, title from pages where text <> ''")
+        # for filter(lambda t: t[1] in retained_pages, rows)
+
+        for text, title in rows:
+            self.send(text, title)
+
+        cur.close()
+
+
+# read only data (assuming copy-on-write semantics)
+WORKER_MATCHES = None
+
+
+class Worker(Actor):
+
+    stats: dict[str, int]
+    spacy_model: str
+
+    def __init__(self, spacy_model):
+        super().__init__()
+        self.spacy_model = spacy_model
+        self.stats = Counter()
+
+        self._tmp_dir = kpath(irt2.ENV.DIR.DATA / 'tmp', create=True)
+        errors = self._tmp_dir / f'{self.name}-errors.gz'
+        self._fd_errors = errors.open(mode='wb')
+
+    def startup(self):
+        log.info(
+            f'[{self.name}] loading spacy pipeline (model={self.spacy_model})')
+
+        self.nlp = spacy.load(
+            self.spacy_model,
+            exclude=[
+                "tagger",
+                "scores",
+                "attribute_ruler",
+                "lemmatizer",
+                "ner",
+            ],
+        )
+
+    def shutdown(self):
+        log.info(f'[{self.name}] writing stats')
+
+        stats = self._tmp_dir / f'{self.name}-stats.yml'
+        with stats.open(mode='w') as fd:
+            yaml.safe_dump(dict(self.stats), fd)
+
+        self._fd_errors.close()
+
+    def recv(self, text, title):
+        doc = self.nlp(text)
+        self.stats['docs handled'] += 1
+
+        # use spacy to detect sentence boundaries
+        bounds, sentences = [], []
+        for sent in doc.sents:
+            if len(sent) <= 1:
+                continue
+
+            bounds.append(sent[-1].idx + len(sent[-1]))
+
+            # joined = " ".join(map(str, sent))
+            # sentences.append(" ".join(joined.split()))
+
+            sentences.append(str(sent))
+
+        # order matches by sentence boundaries
+        matches = sorted(
+            WORKER_MATCHES.get(page=title),
+            key=lambda match: match.end,
+            reverse=True,
+        )
+
+        sentences = list(zip(sentences, bounds))[::-1]
+        if not sentences:
+            self._fd_errors.write(encode_line(
+                ('empty document', title), sep='|'))
+            self.stats['empty documents'] += 1
+            return
+
+        sentence, bound = sentences.pop()
+
+        # do not save multiple matches of a single sentence
+        seen = set()
+        while matches:
+            self.stats['matches handled'] += 1
+
+            match = matches.pop()
+            while bound < match.end:
+                sentence, bound = sentences.pop()
+
+            uniq = (match.eid, match.mention, sentence)
+            if uniq in seen:
+                self.stats['duplicate sentences'] += 1
+                continue
+
+            if match.mention not in sentence:
+                self._fd_errors.write(encode_line(
+                    ('not in mention',) + uniq, sep='|'))
+                self.stats['match not in sentence'] += 1
+                continue
+
+            self.stats['sentences sent'] += 1
+            self.send(
+                match.eid,
+                match.norm_mention,
+                match.mention,
+                sentence,
+            )
+
+            seen.add(uniq)
+
+
+class Writer(Actor):
+
+    out: Union[str, Path]
+    sep: str
+
+    def __init__(self, out, sep):
+        super().__init__()
+        self.out = out
+        self.sep = sep
+
+    def startup(self):
+        log.info(f'[{self.name}] opening {self.out}')
+        self.fd = gzip.open(str(self.out), mode='wb')
+
+    def shutdown(self):
+        self.fd.close()
+
+    def recv(self, *args):
+        encoded = encode_line(args, sep=self.sep)
+        self.fd.write(encoded)
+
+
+def get_text(config, matches: Index[Match], procs: int = 1):
+    global WORKER_MATCHES
+    WORKER_MATCHES = matches
+
+    # read
+
+    reader = Reader(
+        db=irt2.ENV.DIR.ROOT / config['source pages'],
+    )
+
+    # process
+
+    worker = [
+        Worker(spacy_model=config['spacy model'])
+        for _ in range(procs)
+    ]
+
+    # write
+
+    out = irt2.ENV.DIR.ROOT / config['source sentences']
+    kpath(out.parent, create=True)
+    writer = Writer(out=out, sep=config['separator'])
+
+    # dispatch multiprocessing
+
+    relay = Relay(maxsize=100)
+    relay.connect(reader, worker, writer)
+    relay.run()  # blocks
