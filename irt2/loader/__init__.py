3cd3fd623d26872f8f8e4e82164a21d82621b089
Felix Hamann <felix@hamann.xyz>
2024-04-24 17:00:26 +0200

    added control experiments

---
+++ irt2/loader/__init__.py
@@ -0,0 +1,9 @@
+from irt2.loader import blp, irt2
+
+LOADER = {
+    "irt2": irt2.load_irt2,
+    "blp-umls": blp.load_umls,
+    "blp-wn18rr": blp.load_wn18rr,
+    "blp-fb15k237": blp.load_fb15k237,
+    "blp-wikidata5m": blp.load_wikidata5m,
+}
