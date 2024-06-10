30c446631427c5a44fb6690887dd2f08e237044f
Felix Hamann <felix@hamann.xyz>
2024-06-10 16:05:12 +0200

    moved irt2.create to ipynb/lib

--- src/ipynb/lib/ipynb.py
+++ src/ipynb/lib/ipynb.py
@@ -1,18 +1,18 @@
 import logging
 import logging.config
 
-import yaml
-
 import irt2
+import yaml
 
 
 # TODO remove this and use __init__.py:init_logging
 def setup_logging():
     with (irt2.ENV.DIR.CONF / "logging.yaml").open(mode="r") as fd:
         conf = yaml.safe_load(fd)
+        del conf["handlers"]["logfile"]
 
         conf["handlers"]["stdout"]["formatter"] = "plain"
-        conf["loggers"]["root"]["handlers"] = ["stdout"]
+        conf["loggers"]["irt2"]["handlers"] = ["stdout"]
 
         logging.config.dictConfig(conf)
 
